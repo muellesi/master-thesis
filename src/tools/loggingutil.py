2f87cc9ab7f4487200909d2fac561314028951e3
Simon Mueller <simon.mueller@student.kit.edu>
2019-10-23 17:40:09 +0200

    reformat loggingutil.py

--- src/tools/loggingutil.py
+++ src/tools/loggingutil.py
@@ -1,25 +1,26 @@
 import logging
 import sys
 
-def get_logger( name, level = logging.INFO, do_file_logging = True, fileName = "" ):
+
+def get_logger(name, level=logging.INFO, do_file_logging=True, file_name=""):
     logger = logging.getLogger(name)
     logger.propagate = False
-    
+
     formatter = logging.Formatter('%(asctime)-15s (%(module)s:%(lineno)d) %(levelname)s: %(message)s')
-    
+
     if logger.hasHandlers():
         logger.handlers.clear()
-    
+
     logger.addHandler(logging.StreamHandler())
-    
+
     if do_file_logging:
-        if fileName == "":
-            fileName = "{}.log".format(name)
-    
-        logger.addHandler(logging.FileHandler(fileName))
-    
+        if file_name == "":
+            file_name = "{}.log".format(name)
+
+        logger.addHandler(logging.FileHandler(file_name))
+
     for handler in logger.handlers:
         handler.level = level
         handler.formatter = formatter
-        
+
     return logger
