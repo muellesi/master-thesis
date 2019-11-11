0179909e629688cda6e52018f20f18263197cdc7
Simon Mueller <simon.mueller@student.kit.edu>
2019-11-11 21:22:54 +0100

    Adding NYU dataset

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -4,5 +4,5 @@ from .hand_segmentation.simple_hand_segmentation import SimpleHandSegmenter
 from .realsense import RealsenseCamera
 from .realsense import RealsenseSettings
 from .loggingutil import get_logger
-from .datasets import FHAD
+from .datasets import FHAD, NYU
 from .skeleton_renderer import visualize_joints_2d as render_skeleton
