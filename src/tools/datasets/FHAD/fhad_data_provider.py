65a97251f367f7cd2b29c10eadd804d68ed912f4
Simon Mueller <simon.mueller@student.kit.edu>
2019-11-12 13:29:15 +0100

    Adapt NYU tfrecord code for FHAD dataset

--- src/tools/datasets/FHAD/fhad_data_provider.py
+++ src/tools/datasets/FHAD/fhad_data_provider.py
@@ -1,47 +1,134 @@
 import tensorflow as tf
 import os
 import numpy as np
+import math
+import h5py
+import scipy.io as sio
+import shutil
+import cv2
+import progressbar
+import tools
+import tools.datasets.tfrecord_helper as tfrh
+import pathlib
 
+__logger = tools.get_logger(__name__, do_file_logging=False)
+
+
+def prepare_dataset(dataset_location):
+    """"
+        Serializes the FHAD dataset at dataset_location to tfrecord files at dataset_location/tfrecord_data
+    """
+    skeleton_location = os.path.join(dataset_location, "Hand_pose_annotation_v1")
+    img_basepath = os.path.join(dataset_location, "Video_files")
+    tfrecord_basepath = os.path.join(dataset_location, "tfrecord_data")
+
+    train_subjects = ["Subject_1", "Subject_3", "Subject_4"]
+    validate_subjects = ["Subject_2"]
+
+    skeleton_files = [file for file in pathlib.Path(skeleton_location).rglob("skeleton.txt")]
+
+    if os.path.exists(tfrecord_basepath):
+        shutil.rmtree(tfrecord_basepath)
+
+    os.makedirs(tfrecord_basepath)
+
+    train_skel_files = []
+    val_skel_files = []
+    test_skel_files = []
+    for skeleton_file in skeleton_files:
+        relative_path = pathlib.Path(os.path.relpath(str(skeleton_file.absolute()), skeleton_location))
+        subject = relative_path.parts[0]
+        if subject in train_subjects:
+            train_skel_files.append(skeleton_file)
+        elif subject in validate_subjects:
+            val_skel_files.append(skeleton_file)
+        else:
+            test_skel_files.append(skeleton_file)
+
+    all_skel_files = np.array([train_skel_files, val_skel_files, test_skel_files])
+
+    subsets = ["train", "validation", "test"]
+    max_chunk_size = 5000
+
+    progbar = None
+
+    for idx, subset in enumerate(subsets):
+        __logger.info("Creating serialized files for {} set".format(subset))
+        sample_idx = 0
+        chunk_idx = 0
+
+        if progbar: del progbar
+        progbar = progressbar.ProgressBar(widgets=[progressbar.Bar(), progressbar.Percentage(), progressbar.ETA()])
+        progbar.start(max_value=len(all_skel_files[idx]))
+
+        for file_idx, file in enumerate(all_skel_files[idx]):
+
+            skels_world_coords = np.loadtxt(file)
+            specific_img_basepath = os.path.join(img_basepath, file.relative_to(skeleton_location).parent, "depth")
+
+            for line in skels_world_coords:
+                frame_index = int(line[0])
+
+                skeleton = line[1:]
+                skeleton = skeleton.reshape((21, -1))
+                skeleton = tools.skeleton_renderer.project_world_to_cam(skeleton, get_camera_extrinsics())
+                skeleton = skeleton.reshape((-1))
+
+                img_filename = "depth_{:04d}.png".format(frame_index)
+                img_path = os.path.join(specific_img_basepath, img_filename)
+
+                with tf.io.gfile.GFile(img_path, 'rb') as fid:
+                    img = fid.read()
+
+                if sample_idx % max_chunk_size == 0:
+                    # write data to chunk file
+                    filename = "{}_{}.tfrecord".format(subset, chunk_idx)
+                    record_writer = tf.io.TFRecordWriter(os.path.join(tfrecord_basepath, filename))
+                    chunk_idx += 1
+
+                tfrecord_str = tfrh.make_standard_pose_record(sample_idx, img, 640, 480, skeleton)
+                record_writer.write(tfrecord_str)
+                sample_idx += 1
+
+            progbar.update(file_idx)
 
 
 @tf.function
-def __parse_skeleton(skeleton_data_line, file_path):
-    data_elems = tf.strings.split(skeleton_data_line, " ")
-    return file_path, data_elems[0], tf.strings.to_number(data_elems[1:], out_type=tf.dtypes.float32)
+def __decode_img(index, depth, img_width, img_height, skeleton):
+    img = tf.image.decode_png(depth, channels=1, dtype=tf.uint16)
+    return img, skeleton
 
 
 @tf.function
-def __read_skeleton_file(file_path):
-    skeleton_vals = tf.data.TextLineDataset(file_path).map(lambda x: __parse_skeleton(x, file_path))
-    return skeleton_vals
+def __open_tf_record(filename):
+    tfrecord = tf.data.TFRecordDataset(filename)
+    return tfrecord.map(tfrh.parse_standard_pose_record)
 
 
-@tf.function
-def __get_corresponding_images(skeleton_path, frame_index, skeleton):
-    video_path = tf.strings.regex_replace(skeleton_path, "Hand_pose_annotation_v1", "Video_files")
-    path_length = tf.strings.length(video_path)
-    video_path = tf.strings.substr(video_path, 0, path_length - len("skeleton.txt"))
-    path_add = tf.constant("depth/depth_")
-    file_ending = tf.constant(".png")
-    img_path = tf.strings.join([video_path, path_add, frame_index, file_ending])
-    return tf.io.decode_png(tf.io.read_file(img_path), dtype=tf.dtypes.uint16), skeleton
-
-
-def get_dataset(path, subset='train', img_dim=None):
+def get_dataset(dataset_location, subset='train'):
     """"
         Returns a tf.data.DataSet for the FHAD dataset
-        Dataset source: https://arxiv.org/abs/1704.02463 "First-Person Hand Action Benchmark with RGB-D Videos and 3D Hand Pose Annotations"
-        :param path: root path to the dataset
-        :param img_dim: tuple (height, width) of the requested data set
+
+        Dataset source: https://guiggh.github.io/publications/first-person-hands/
+        "First-Person Hand Action Benchmark with RGB-D Videos and 3D Hand Pose Annotations"
+        Garcia-Hernando, Guillermo and Yuan, Shanxin and Baek, Seungryul and Kim, Tae-Kyun.
+        Proceedings of Computer Vision and Pattern Recognition ({CVPR}), 2018
+
+        :param subset: 'train', 'validation' or 'test'
+        :param dataset_location: root path to the FHAD dataset
     """
-    list_ds = tf.data.Dataset.list_files(os.path.join(path, '*/skeleton.txt')).cache()
-    skeleton_data = list_ds.flat_map(__read_skeleton_file).cache()
-    full_ds = skeleton_data.map(__get_corresponding_images, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    if img_dim is not None:
-        full_ds = full_ds.map(lambda img, skel: (tf.image.resize(img, tf.constant([img_dim[0], img_dim[1]], dtype=tf.dtypes.int32)), skel),
-                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    # train_ds = full_ds.filter(lambda path, _, _: any(tf.strings.))
-    return full_ds
+    tfrecord_basepath = os.path.abspath(os.path.join(dataset_location, 'tfrecord_data'))
+
+    if not os.path.exists(tfrecord_basepath):
+        prepare_dataset(dataset_location)
+
+    filenames = np.array([os.path.abspath(os.path.join(tfrecord_basepath, f)) for f in os.listdir(tfrecord_basepath) if f.startswith(subset)])
+
+    dataset = (tf.data.Dataset.from_tensor_slices(filenames).cache()
+               .flat_map(__open_tf_record)
+               .map(__decode_img, num_parallel_calls=tf.data.experimental.AUTOTUNE))
+
+    return dataset
 
 
 def get_joint_names():
